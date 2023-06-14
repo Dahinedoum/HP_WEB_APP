70f9d73772b82cddcd1b55d2708e679e8aa1d3a7
Reyeslim <reyeslimones@gmail.com>
2023-06-14 14:50:10 +0200

    feat: implemented Image component and categories added to landing view

--- src/views/Landing/landing.tsx
+++ src/views/Landing/landing.tsx
@@ -1,7 +1,15 @@
 import { FC, memo } from 'react'
-import { LandingContainer, LandingContent } from './landingStyles'
+import {
+  CategoryContainer,
+  CategoryContent,
+  CategoryHeaderContainer,
+  LandingContainer,
+  LandingContent,
+  Button,
+} from './landingStyles'
 import Header from '../../components/Header/header'
 import Footer from '../../components/Footer/footer'
+import Image from '../../components/Image/image'
 import { useNavigate } from 'react-router-dom'
 import type { Props } from './landingTypes'
 
@@ -9,7 +17,34 @@ const Landing: FC<Props> = ({}) => {
   return (
     <LandingContainer>
       <Header />
-      <LandingContent></LandingContent>
+      <LandingContent>
+        <CategoryContainer>
+          <CategoryContent>
+            <CategoryHeaderContainer>
+              <Image src="/hogwarts-5078578_1920.jpg" alt="" />
+            </CategoryHeaderContainer>
+            <Button>View All Characters</Button>
+          </CategoryContent>
+          <CategoryContent>
+            <CategoryHeaderContainer>
+              <Image src="/travel-3238442_1920.jpg" alt="" />
+            </CategoryHeaderContainer>
+            <Button>View Students</Button>
+          </CategoryContent>
+          <CategoryContent>
+            <CategoryHeaderContainer>
+              <Image src="/library-948965_1920.jpg" alt="" />
+            </CategoryHeaderContainer>
+            <Button>View Staff</Button>
+          </CategoryContent>
+          <CategoryContent>
+            <CategoryHeaderContainer>
+              <Image src="/harry-potter-2348767_1920.jpg" alt="" />
+            </CategoryHeaderContainer>
+            <Button>View Spells</Button>
+          </CategoryContent>
+        </CategoryContainer>
+      </LandingContent>
       <Footer />
     </LandingContainer>
   )
